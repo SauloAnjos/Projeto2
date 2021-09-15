d7aac4430305a21d8ed9ff036465c472421f7141
SauloAnjos <saulo.anjos@gmail.com>
2021-09-15 12:11:25 -0300

    Segundo Projeto

---
+++ obj/Debug/net5.0/ExemploApiCatalogoJogos.AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("ExemploApiCatalogoJogos")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("ExemploApiCatalogoJogos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ExemploApiCatalogoJogos")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
